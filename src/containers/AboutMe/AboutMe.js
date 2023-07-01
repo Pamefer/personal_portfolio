57c77cd9261a54f849fdc76b374af021ad6fdad4
Pamela Guaman <pamelaguaman@mbp-de-pamela.mynetworksettings.com>
2023-06-30 23:11:38 -0400

    Add home and about me sections

---
+++ src/containers/AboutMe/AboutMe.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './AboutMe.css';
+
+const AboutMe = () => {
+  return (
+    <div className='app__aboutus app__wrapper flex__center section__padding' id="about">
+        <p>Software Engineer with over 5 years of experience in frontend and backend projects.
+           I am a senior technologist with experience in developing large 
+           application systems. I am highly skilled in software development
+            and stakeholder management. I have worked in the end-to-end
+            development process. I have worked on agile cross-functional 
+            teams. I’m always willing to learn, cultivate and collaborate 
+            with others.</p>
+    </div>
+  )
+}
+
+export default AboutMe
