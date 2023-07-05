1a70cb8ee92a6df4ccef0d13ee44d6c79fb96054
Pamela Guaman <pamefer.95@gmail.com>
2023-07-05 11:21:18 -0400

    End about me session add styles

--- src/containers/AboutMe/AboutMe.js
+++ src/containers/AboutMe/AboutMe.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import './AboutMe.css';
 
 const AboutMe = () => {
+  const [yearsExperience, setYearsExperience]=useState(1);
+  useEffect(() => {
+    getYear();
+  }, [])
+
+  const getYear=()=>{
+    const INIT_WORK=2018;
+    let actualYear= new Date().getFullYear(); 
+    setYearsExperience(actualYear-INIT_WORK)
+  }
+
+  
   return (
-    <div className='app__aboutus app__wrapper flex__center section__padding' id="about">
-        <p>Software Engineer with over 5 years of experience in frontend and backend projects.
-           I am a senior technologist with experience in developing large 
-           application systems. I am highly skilled in software development
-            and stakeholder management. I have worked in the end-to-end
-            development process. I have worked on agile cross-functional 
-            teams. I’m always willing to learn, cultivate and collaborate 
-            with others.</p>
+    <div className='app__aboutus app__wrapper_column flex__center section__padding' id="about">
+
+        <p className='p__cormorant app__aboutus-p-tittle'>Frontend, Backend, Consultant </p>
+
+        <p className='p__opensans'>I am highly skilled in software development and stakeholder management</p>
+        <p className='p__opensans'>With over {yearsExperience} years of experience </p>
     </div>
   )
 }
