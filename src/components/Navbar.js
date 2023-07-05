1efb3cc3d1c29e16690f6eb81e6cbcd651779cbd
Pamela Guaman <pamefer.95@gmail.com>
2023-07-05 11:29:20 -0400

    End about me session add styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,10 +26,11 @@ const Navbar = () => {
             <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
               <FcCloseUpMode fontSize={27} className='overlay__close' onClick={()=>setToggleMenu(false)}/>
               <ul className='app__navbar-smallscreen_links'>
-                <li className="p__opensans"><a href="#home">About me</a> </li>
-                <li className="p__opensans"><a href="#work">Work</a> </li>
-                <li className="p__opensans"><a href="#interest">Interest</a> </li>
-                <li className="p__opensans"><a href="#feedback">Feedback</a> </li>
+                <li className="p__opensans"><a href="#home" onClick={()=>setToggleMenu(false)}>Home</a> </li>
+                <li className="p__opensans"><a href="#about" onClick={()=>setToggleMenu(false)}>About</a> </li>
+                <li className="p__opensans"><a href="#work" onClick={()=>setToggleMenu(false)}>Work</a> </li>
+                <li className="p__opensans"><a href="#interest" onClick={()=>setToggleMenu(false)}>Interest</a> </li>
+                <li className="p__opensans"><a href="#feedback" onClick={()=>setToggleMenu(false)}>Feedback</a> </li>
               </ul>
           </div>
           )} 
