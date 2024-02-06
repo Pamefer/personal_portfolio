7f2ab48fbefac083cbf26d903b2335185ffa78a2
Pamela Guaman <pamefer.95@gmail.com>
2024-02-06 00:43:16 -0500

    fix: change description about me and skills

--- src/containers/AboutMe/AboutMe.js
+++ src/containers/AboutMe/AboutMe.js
@@ -1,37 +1,37 @@
-import React,{useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import './AboutMe.css';
 
 const AboutMe = () => {
-  const [yearsExperience, setYearsExperience]=useState(1);
+  const [yearsExperience, setYearsExperience] = useState(1);
   useEffect(() => {
     getYear();
   }, [])
 
-  const getYear=()=>{
-    const INIT_WORK=2018;
-    let actualYear= new Date().getFullYear(); 
-    setYearsExperience(actualYear-INIT_WORK)
+  const getYear = () => {
+    const INIT_WORK = 2018;
+    let actualYear = new Date().getFullYear();
+    setYearsExperience(actualYear - INIT_WORK)
   }
 
-  
+
   return (
     <div className='app__aboutus app__wrapper_column flex__center section__padding' id="about">
 
-        <p className='p__cormorant app__aboutus-p-tittle'>Frontend, Backend, Consultant Developer </p>
+      <p className='p__cormorant app__aboutus-p-tittle'>Frontend, Backend, Consultant Developer </p>
 
-        <p className='p__opensans'>I am highly skilled in software development and stakeholder management</p>
-        <div className='about_container' >
-          <div className='aboutleft'>
-            <p className='p__opensans_aboutme'>With over {yearsExperience} years of experience creating bold, future-focused technical solutions for clients.</p>
-            <p className='p__opensans_aboutme'>As a consultant I have managed to maintain solid relationships with people outside the development team, clients and other stakeholders by managing clear business communication.</p>
-          </div>
-          <div className='aboutright'>
-            <p className='p__opensans_aboutme'>I have worked in agile, inclusive and autonomous collaborative teams.</p>
-            <p className='p__opensans_aboutme'>Seeking and giving feedback has been essential for my professional and team growth.</p>
-          </div>
+      <p className='p__opensans'>I am highly skilled in software development and stakeholder management</p>
+      <div className='about_container' >
+        <div className='aboutleft'>
+          <p className='p__opensans_aboutme'>With over {yearsExperience} years of experience creating bold, future-focused technical solutions with the best software practices.</p>
+          <p className='p__opensans_aboutme'>As a consultant I have managed to maintain solid relationships with people outside the development team, clients and other stakeholders by managing clear business communication.</p>
         </div>
-        
-  </div>
+        <div className='aboutright'>
+          <p className='p__opensans_aboutme'>I have worked in agile, inclusive and autonomous collaborative teams.</p>
+          <p className='p__opensans_aboutme'>Seeking and giving feedback has been essential for my professional and team growth.</p>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
--- src/containers/Experience/Experience.js
+++ src/containers/Experience/Experience.js
@@ -1,50 +1,56 @@
-import React,{useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import './Experience.css';
 
-const experienceData=[
+const experienceData = [
+  {
+    company: 'Senirop',
+    web: 'https://senirop.com/',
+    date: 'October 2023 – December 2023',
+    titles: ['Software Engineer', 'Frontend Developer']
+  },
   {
     company: 'Thoughtworks',
-    web:'https://www.thoughtworks.com/',
-    date:'January 2019 – June 2023',
-    tittles:['Software Engineer','Fullstack Developer','Consultant']
+    web: 'https://www.thoughtworks.com/',
+    date: 'January 2019 – June 2023',
+    titles: ['Software Engineer', 'Fullstack Developer', 'Consultant']
   },
   {
     company: 'IOET',
-    web:'https://www.ioet.com/',
+    web: 'https://www.ioet.com/',
     date: 'September 2018 - December 2018',
-    tittles: ['Software Engineer','Frontend Developer']
+    titles: ['Software Engineer', 'Frontend Developer']
   },
   {
-    company: 'University Tecnica Particular de Loja',
-    web:'https://www.utpl.edu.ec/',
+    company: 'UTPL College',
+    web: 'https://www.utpl.edu.ec/',
     date: 'January 2018 - April 2018',
-    tittles: ['Software Engineer','Artificial Intelligence', 'Researcher']
+    titles: ['Software Engineer', 'Artificial Intelligence', 'Researcher']
   }
 ]
 
 const Experience = () => {
-  const [experience, setExperience]=useState([]);
-  useEffect(()=>{
+  const [experience, setExperience] = useState([]);
+  useEffect(() => {
     setExperience(experienceData)
-  },[])
+  }, [])
 
   return (
     <div className='app__aboutus app__wrapper_column flex__center section__padding' id="experience">
 
       <div className='container_experience'>
-        {experience.map((e, eNum)=>(
+        {experience.map((e, eNum) => (
           <div key={eNum}>
-            <p className='p__opensans_aboutme'><b><a href={e.web}>{e.company}</a></b></p>
+            <p className='p__opensans_aboutme'><b><a target='_blank' rel="noreferrer" href={e.web}>{e.company}</a></b></p>
             <p className='p__opensans_aboutme'>{e.date}</p>
             <div className='titles_container'>
-              {e.tittles.map((title,num )=>(
-                <p className='p__opensans_aboutme_bold' style={{fontWeight:'900'}} key={num}>{title}</p>
+              {e.titles.map((title, num) => (
+                <p className='p__opensans_aboutme_bold' style={{ fontWeight: '900' }} key={num}>{title}</p>
               ))}
             </div>
           </div>
         ))}
       </div>
-    
+
     </div>
   )
 }
