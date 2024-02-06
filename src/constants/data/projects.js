da5ad6b80e4c55ae15d01f08d897f2a73e8f198f
Pamela Guaman <pamefer.95@gmail.com>
2024-02-06 09:51:28 -0500

    feature: described projects for info data

--- src/constants/data/projects.js
+++ src/constants/data/projects.js
@@ -1,32 +1,116 @@
 const experienceData = [
-    {
-        company: 'Senirop',
-        web: 'https://senirop.com/',
-        date: 'October 2023 – December 2023',
-        titles: ['Software Engineer', 'Frontend Developer'],
-        product: ["Tapinto Digital Newspaper"]
-    },
-    {
-        company: 'Thoughtworks',
-        web: 'https://www.thoughtworks.com/',
-        date: 'January 2019 – June 2023',
-        titles: ['Software Engineer', 'Fullstack Developer', 'Consultant'],
-        product: ["Beauty Counter, Comissions", "A Place For Mom, Callcenter", "Latam, Login"]
-    },
-    {
-        company: 'IOET',
-        web: 'https://www.ioet.com/',
-        date: 'September 2018 - December 2018',
-        titles: ['Software Engineer', 'Frontend Developer'],
-        product: ["Internal app for managing stock"]
-    },
-    {
-        company: 'UTPL College',
-        web: 'https://www.utpl.edu.ec/',
-        date: 'January 2018 - April 2018',
-        titles: ['Software Engineer', 'Artificial Intelligence', 'Researcher'],
-        product: ["Identity validation for remote students"]
-    }
+  {
+    company: 'Pie for Providers',
+    web: 'https://pieforproviders.com/',
+    date: 'August 2023 – Actual',
+    titles: ['Technitian'],
+    product: [
+      {
+        name: "Pie for providers",
+        web: "https://pieforproviders.com/",
+        description: [
+          "Wrote python scripts to automate manual tasks like formatting, reducing 95% time",
+        ],
+        techStack: ["Python", "Numpy", "Pandas"]
+      }]
+  },
+  {
+    company: 'Senirop',
+    web: 'https://senirop.com/',
+    date: 'October 2023 – December 2023',
+    titles: ['Software Engineer', 'Frontend Developer'],
+    product: [
+      {
+        name: "Tapinto Digital Newspaper",
+        web: "tapinto.com",
+        description: [
+          "Digital newspaper for New Jersey cities",
+          "Led the development for responsive, faster, and scalable Milestones and Directories webpages of the newslatter",
+          "Redisgned old site, built new one!"
+        ],
+        techStack: ["React", "Javascript", "Ruby and rails"]
+      }]
+  },
+  {
+    company: 'Thoughtworks',
+    web: 'https://www.thoughtworks.com/',
+    date: 'January 2019 – June 2023',
+    titles: ['Software Engineer', 'Fullstack Developer', 'Consultant'],
+    product: [
+      {
+        name: "Beauty Counter, Commissions",
+        web: "beautycounter.com",
+        description: [
+          "Cosmetic Retail Company",
+          "Involved on the Commisions Team for manteinance and creating new features",
+          "Designed feautures backend and frontend for manual tasks reducing 100% manual work",
+          "Led and migrated CI pipeline from Teamcity to Github Actions, improving the delivery time in 60% less",
+          "Led agile ceremonies and facilitated technical showcases"
+        ],
+        techStack: ["React", "Javascript", "Node", "GCP", "AWS", "C#", "SQL", "Jest"]
+      },
+      {
+        name: "A Place For Mom, Callcenter",
+        web: "aplaceformom.com",
+        description: [
+          "Senior Care Referral Company",
+          "Involved on the migration for a new dynamic and modular callcenter app improving 50% of satisfaction",
+          "Collaborated closely with stakeholders to map their objectives in technical solutions",
+          "Developed storybook for styled frontend components to reuse",
+          "Implemented admin callcenter and callcenter feature (backend and frontend)",
+          "Integrated third party libraries for phonecalls and analytics"
+        ],
+        techStack: ["React", "Redux", "Typescript", "Node", "GraphQL", "AWS", "PostgresSQL", "Jest", "Test React Library"]
+      },
+      {
+        name: "Latam, Login",
+        web: "latam.com",
+        description: [
+          "Latin America Airline",
+          "Involved on the authentication process team: login, registration, password update.",
+          "Mantained legacy services (frontend and backend)",
+          "Participated on inception for new product",
+          "Migrated legacy login to use auth0 including social login (backend and frontend)",
+          "Created internal app to switch on/off manteinance page for registration page"
+        ],
+        techStack: ["React", "Storybook", "Typescript", "Node", "Java", "AWS", "Jenkins", "Jest"]
+      },
+    ]
+  },
+  {
+    company: 'IOET',
+    web: 'https://www.ioet.com/',
+    date: 'September 2018 - December 2018',
+    titles: ['Software Engineer', 'Frontend Developer'],
+    product: [
+      {
+        name: "Internal app for managing stock",
+        description: [
+          "Fixed issues and bugs from legacy features",
+          "Added new features for admin managment of the app"
+        ],
+        techStack: ["AngularJS", "Javacript", "Mocca"]
+      }
+    ]
+  },
+  {
+    company: 'UTPL College',
+    web: 'https://www.utpl.edu.ec/',
+    date: 'January 2018 - April 2018',
+    titles: ['Software Engineer', 'AI Engineer', 'Researcher'],
+    product: [
+      {
+        name: "PAIEEL",
+        description: [
+          "Validation students identities app for remote students when taking online assesments",
+          "Researched and trained algorithms of face recognition",
+          "Developed 3 apps for the purpose: 1. stundnts app 2. teacher app 3. admin app"
+        ],
+        techStack: ["Django", "Python", "OpenFace", "Redis", "PostgreSQL"]
+      },
+
+    ]
+  }
 ]
 
 export default experienceData;
