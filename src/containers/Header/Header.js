f744f086756d9ffb4579ca329d7dc80421568cbd
Pamela Guaman <pamefer.95@gmail.com>
2024-02-06 01:34:38 -0500

    fix: add linkedin and projects

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -5,13 +5,15 @@ import profile from '../../constants/images/9.png';
 const Header = () => {
   return (
     <div className='app__header app__wrapper section__padding' id="home">
-        <div className='app__wrapper_info'>
-            <p className='p__cormorant-header'>I'm Pamela Guamán</p>
-            <h1 className='app__header'>Software Engineer</h1>
-           </div>
-        <div className='app__wrapper_img'>
-          <img src={profile} alt="pamela" className='my-image__img'/>
-        </div>
+      <div className='app__wrapper_info'>
+        <p className='p__cormorant-header'>I'm Pamela Guamán</p>
+        <h1 className='app__header'>Software Engineer</h1>
+        <p className='p__opensans_link_bold'><b><a target='_blank' rel="noreferrer" href="www.linkedin.com/in/pammefer/">linkedin.com/in/pammefer/</a></b></p>
+
+      </div>
+      <div className='app__wrapper_img'>
+        <img src={profile} alt="pamela" className='my-image__img' />
+      </div>
 
     </div>
   )
