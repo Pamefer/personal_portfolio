bab4abc43e6c04b03b730097f2d78e63d833be3b
Pamela Guaman <pamefer.95@gmail.com>
2024-02-07 00:32:06 -0500

    feature: fix link linkedin

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
       <div className='app__wrapper_info'>
         <p className='p__cormorant-header'>I'm Pamela Guamán</p>
         <h1 className='app__header'>Software Engineer</h1>
-        <p className='p__opensans_link_bold'><b><a target='_blank' rel="noreferrer" href="www.linkedin.com/in/pammefer/">linkedin.com/in/pammefer/</a></b></p>
+        <p className='p__opensans_link_bold pointer'><b><a target='_blank' rel="noreferrer" href="https://www.linkedin.com/in/pammefer/">linkedin.com/in/pammefer/</a></b></p>
 
       </div>
       <div className='app__wrapper_img'>
